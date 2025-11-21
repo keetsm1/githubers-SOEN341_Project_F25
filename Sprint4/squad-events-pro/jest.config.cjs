abcad80a0f8f123adf36e3f186788392179c1729
Arnav Singh <159664776+arnav-singh-ahlawat@users.noreply.github.com>
2025-11-21 08:23:30 -0500

    Create jest.config.cjs

---
+++ Sprint4/squad-events-pro/jest.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+
+    roots: ['<rootDir>/src'],
+
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+
+    setupFilesAfterEnv: ['<rootDir>/src/test-utils/setupTests.ts'],
+
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.json'
+        }
+    },
+
+    collectCoverageFrom: [
+        'src/**/*.{ts,tsx}',
+        '!src/main.tsx',
+        '!src/vite-env.d.ts'
+    ]
+};
